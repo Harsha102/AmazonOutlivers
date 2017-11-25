c13f97528b91e22b23edb4e9be879949c6f7262d
ksskreddy <ksskreddy2015@gmail.com>
2017-11-25 10:11:42 +0530

    Update TreeController.js

--- Assets/assets/TreeChopping/TreeController.js
+++ Assets/assets/TreeChopping/TreeController.js
@@ -16,28 +16,28 @@ function Start()
 
 function Update()
 {
-	if(treeHealth <= 0)
-	{
-		GetComponent.<Rigidbody>().isKinematic = false;
-		GetComponent.<Rigidbody>().AddForce(transform.forward * speed);
-		DestroyTree();
-	}
+    if(treeHealth <= 0)
+    {
+        GetComponent.<Rigidbody>().isKinematic = false;
+        GetComponent.<Rigidbody>().AddForce(transform.forward * speed);  //Added a force to make tree fall down to ground
+        DestroyTree();
+    }
 }
 
 function DestroyTree()
 {
-	yield WaitForSeconds(7);
-	Destroy(tree);
-	
-	var position : Vector3 = Vector3(Random.Range(-1.0, 1.0), 0, Random.Range(-1.0, 1.0));
-	Instantiate(logs, tree.transform.position + Vector3(0,0,0) + position, Quaternion.identity);
-	Instantiate(logs, tree.transform.position + Vector3(2,2,0) + position, Quaternion.identity);
-	Instantiate(logs, tree.transform.position + Vector3(5,5,0) + position, Quaternion.identity);
-	
-	Instantiate(coconut, tree.transform.position + Vector3(0,0,0) + position, Quaternion.identity);
-	Instantiate(coconut, tree.transform.position + Vector3(2,2,0) + position, Quaternion.identity);
-	Instantiate(coconut, tree.transform.position + Vector3(5,5,0) + position, Quaternion.identity);
-	
+    yield WaitForSeconds(7);     //wait for 7 seconds and then convert it to logs and coconuts
+    Destroy(tree);                  //destroy the tree object
+    
+    var position : Vector3 = Vector3(Random.Range(-1.0, 1.0), 0, Random.Range(-1.0, 1.0));
+    //Instantiate log,coconut objects
+    Instantiate(logs, tree.transform.position + Vector3(0,0,0) + position, Quaternion.identity);
+    Instantiate(logs, tree.transform.position + Vector3(2,2,0) + position, Quaternion.identity);
+    Instantiate(logs, tree.transform.position + Vector3(5,5,0) + position, Quaternion.identity);
+    
+    Instantiate(coconut, tree.transform.position + Vector3(0,0,0) + position, Quaternion.identity);
+    Instantiate(coconut, tree.transform.position + Vector3(2,2,0) + position, Quaternion.identity);
+    Instantiate(coconut, tree.transform.position + Vector3(5,5,0) + position, Quaternion.identity);
+    
 }
-
 
